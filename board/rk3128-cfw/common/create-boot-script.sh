ee17233cfadbd8caa6ece37badf9a7c2b99286e5
acmeplus <ctalon@gmail.com>
2021-12-14 04:17:45 +0000

    Added initial SD card config (WIP)
    - Added create-boot-script.sh to create the boot structure (adapted from
      batocera.linux)
    - Added post-build.sh to crate the necessary idbloader.bin, trust.img,
      etc.
    - Added genimage.cfg to configure the sdcard image structure
    - Added rockchip rkbin package needed to create uboot.img

---
+++ board/rk3128-cfw/common/create-boot-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# BOARD_DIR = board specific dir
+# BINARIES_DIR = images dir
+
+BOARD_DIR=$1
+BINARIES_DIR=$2
+
+mkdir -p "${BINARIES_DIR}/boot"     || exit 1
+mkdir -p "${BINARIES_DIR}/extlinux" || exit 1
+
+cp "${BINARIES_DIR}/zImage"             "${BINARIES_DIR}/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"          "${BINARIES_DIR}/boot/"                || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs"    "${BINARIES_DIR}/boot/batocera"        || exit 1
+
+cp "${BINARIES_DIR}/rk3128-wx8.dtb"     "${BINARIES_DIR}/boot/"     || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"    "${BINARIES_DIR}/extlinux/" || exit 1
+
+exit 0
---
+++ board/rk3128-cfw/common/post-build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+MKIMAGE=$HOST_DIR/bin/mkimage
+BOARD_DIR="$(dirname $0)"
+UBOOT_DIR=output/build/uboot-wx8
+IMAGES_DIR=output/images
+
+$UBOOT_DIR/tools/mkimage -n rk3128 -T rksd -d $IMAGES_DIR/rkbin/bin/rk31/rk3128_ddr_300MHz_v2.12.bin $IMAGES_DIR/idbloader.img
+cat $IMAGES_DIR/rkbin/bin/rk31/rk3128x_miniloader_v2.57.bin >> $IMAGES_DIR/idbloader.img
+
+# Generate uboot.img
+$UBOOT_DIR/tools/loaderimage --pack --uboot $UBOOT_DIR/u-boot-dtb.bin $IMAGES_DIR/uboot.img --size 1024 4
+  
+# Generate trust.img
+$UBOOT_DIR/tools/loaderimage --pack --trustos $IMAGES_DIR/rkbin/bin/rk31/rk3126_tee_ta_v2.01.bin $IMAGES_DIR/trust.img --size 1024 4
+
+board/rk3128-cfw/common/create-boot-script.sh $BOARD_DIR $IMAGES_DIR 
+
+install -m 0644 -D $BOARD_DIR/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
