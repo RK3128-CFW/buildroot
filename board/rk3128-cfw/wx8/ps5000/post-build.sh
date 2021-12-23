ea71e137b6606a100fca2141434421e5a25df6a1
acmeplus <ctalon@gmail.com>
2021-12-23 20:12:15 +0000

    Added additional fixes for WX8/PS5000
    - Updated populateshare to add the retroarch.cfg file to the right path
      (/userdata/system/.config/retroarch/retroarch.cfg)
    - Updated dbus wifi service to call an external wpa-supplicant
    - Added template wpa-supplicant.conf file (add your own SSID/key)
    - Added wx8 joystick init script to enable analogue joysticks
    - Additional buildroot configuration changes

--- board/rk3128-cfw/wx8/ps5000/post-build.sh
+++ board/rk3128-cfw/wx8/ps5000/post-build.sh
@@ -16,8 +16,10 @@ $UBOOT_DIR/tools/loaderimage --pack --uboot $UBOOT_DIR/u-boot-dtb.bin $IMAGES_DI
 $UBOOT_DIR/tools/loaderimage --pack --trustos $IMAGES_DIR/rkbin/bin/rk31/rk3126_tee_ta_v2.01.bin $IMAGES_DIR/trust.img --size 1024 4
 
 # Populate the boot partition
-board/rk3128-cfw/common/create-boot-script.sh $BOARD_DIR $IMAGES_DIR 
+board/rk3128-cfw/wx8/ps5000/create-boot-script.sh $BOARD_DIR $IMAGES_DIR 
 install -m 0644 -D $BOARD_DIR/boot/extlinux.conf $TARGET_DIR/boot/extlinux/extlinux.conf
+install -m 0644 -D board/rk3128-cfw/common/wpa-supplicant.conf $IMAGES_DIR/wpa-supplicant.conf
+install -m 0644 -D ../package/batocera/core/batocera-system/batocera-boot.conf $IMAGES_DIR/batocera-boot.conf
 
 # Create overlay for 
 if [ ! -d $TARGET_DIR/overlay ]; then
